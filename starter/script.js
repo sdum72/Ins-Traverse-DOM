e8572ac62472eb317cba9e5c8df9d31454f06e64
sdum72 <sdum@hotmail.com>
2023-11-30 07:10:56 +0000

    Practice Exercises Completed

---
+++ starter/script.js
@@ -0,0 +1,15 @@
+// TODO: What will the following line of code log?
+console.log(window);  // logs window object
+
+// TODO: What will the following line of code log?
+console.log(window.document);  // logs reference to document in the window object
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement);  // logs root element of document
+
+// // TODO: What will the following line of code log?
+console.log(document.head);  // returns head element of current document
+
+// How would you log the `length` property of the `window` object?
+console.log(window.length)  // returns the length property of the window object
+
